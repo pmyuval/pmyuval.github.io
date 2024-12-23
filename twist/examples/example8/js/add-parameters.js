b31a471c9d8ca577a425cb0f5365bbf6304c519c
coYuvalpm <pundakmintz1@mail.tau.ac.il>
2024-12-23 02:00:32 +0200

    update examples page with simple example

---
+++ twist/examples/example8/js/add-parameters.js
@@ -0,0 +1,15 @@
+function addParameters(a, b){
+    console.log("Results: " + a + b);
+}
+
+function addStrings(){
+    let a = "hello";
+    let b = "world";
+    addParameters(a, b);
+}
+
+function addNumbers(){
+    let a = 5;
+    let b = 7;
+    addParameters(a, b);
+}
